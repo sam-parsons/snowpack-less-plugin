1b3197bfc0d372882d0d7ad83f4303fee9cd2223
Sam Parsons <samparsons269@gmail.com>
2020-12-21 17:28:19 -0800

    added testing for imports, currently failing

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -1,13 +1,19 @@
 const plugin = require('../plugin.js');
 const path = require('path');
 
-const pathToLessApp = path.join(__dirname, 'fixtures/styles.less');
+const pathToLessBasic = path.join(__dirname, 'fixtures/basic/styles.less');
+const pathToLessImports = path.join(__dirname, 'fixtures/imports/styles1.less');
 
 describe('snowpack-less-plugin', () => {
   test('returns the compiled Less result', async () => {
     const p = plugin(null, {});
-    const lessResult = await p.load({filePath: pathToLessApp, isDev: false});
-    console.log(lessResult);
+    const lessResult = await p.load({filePath: pathToLessBasic, isDev: false});
+    expect(lessResult).toMatchSnapshot('styles.less');
+  });
+
+  test('handles imports in Less', async () => {
+    const p = plugin(null, {});
+    const lessResult = await p.load({filePath: pathToLessImports, isDev: false});
     expect(lessResult).toMatchSnapshot('styles.less');
   });
 });
